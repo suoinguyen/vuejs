0787a1f5e5427b1571981e5b7dcf8ab844228391
suoi.nguyen <suoi.nguyen@hdwebsoft.co>
2017-10-31 00:12:23 +0700

    Startbase

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <div class="container">
+      <div class="row">
+        <Character 
+        v-for="(id, index) in initial_ids" 
+        v-bind:id="id" 
+        v-bind:index="index" 
+        v-bind:key="index"
+        v-on:removeCharacterParent="removeCharacter"
+        ></Character>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Character from "./components/Character.vue"
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Starbase App!',
+      initial_ids: [1, 10, 12]
+    }
+  },
+  components:{
+    Character
+  },
+  methods:{
+    removeCharacter(index){
+      this.initial_ids.splice(index, 1);
+    }
+  }
+}
+</script>
+
---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="col-md-4">
+        <div class="card">
+            {{id}}
+            <div class="card-block">
+                <button class="close" v-on:click="removeCharacterChild(index)">&times;</button>
+                <h4 class="card-title">{{character.name}}</h4>
+                <p class="card-subtitle">{{character.birth_year}}</p>
+                <p class="card-text">{{character.height}}</p>
+                <p class="card-text">{{character.mass}}</p>
+                <p class="card-text">{{character.hair_color}}</p>
+                <p class="card-text">{{character.eye_color}}</p>
+                <button class="btn btn-primary" v-on:click="switchCharacter">switchCharacter</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['id', 'index'],
+        data() {
+            return {
+                character: ''
+            }
+        },
+        methods: {
+            fetchCharacterInfo(id){
+                //using extension Allow-Control-Allow-Origin: * - Chrome Web Store to disable security.
+                fetch(`https://swapi.co/api/people/${id}`, {
+                    method: 'GET',
+                })
+                .then(response => response.json())
+                .then(character_info => this.character = character_info)
+            },
+            switchCharacter(){
+                var id = Math.floor(Math.random()*83)+1;
+                this.fetchCharacterInfo(id);
+            },
+            removeCharacterChild(index){
+                this.$emit('removeCharacterParent', index);
+                this.fetchCharacterInfo(this.id);
+            }
+        },
+        created(){
+            this.fetchCharacterInfo(this.id);
+        },
+    }
+</script>
+<style>
+    .card{
+        background: transparent;
+        border: solid 1px #ccc;
+        margin-top: 15px;
+    }
+</style>
