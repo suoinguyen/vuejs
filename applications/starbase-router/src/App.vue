35983f01aa4a0b206f455218106ba593b2e6ac19
suoi.nguyen <suoi.nguyen@hdwebsoft.co>
2017-11-03 15:31:39 +0700

    add statrbase router

---
+++ applications/starbase-router/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name : 'App',
+  data(){
+      return {
+          title: 'Starbase Router!'
+      }
+  }
+}
+</script>
---
+++ applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h4>{{message}}</h4>
+      
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return {
+          message: 'hehehe'
+      }
+  }
+}
+</script>
